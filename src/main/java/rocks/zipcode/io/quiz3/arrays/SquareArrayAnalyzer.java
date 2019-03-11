a462b4b2c8a27fe7b668438fb3e023abf1e92911
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-03-11 12:38:48 -0400

    refactored SquareArrayAnalyzer.java

--- src/main/java/rocks/zipcode/io/quiz3/arrays/SquareArrayAnalyzer.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/SquareArrayAnalyzer.java
@@ -1,11 +1,17 @@
 package rocks.zipcode.io.quiz3.arrays;
 
+import java.util.Arrays;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class SquareArrayAnalyzer {
     public static Boolean compare(Integer[] input, Integer[] squaredValues) {
-        // TODO returning true passes all the tests, but still return and actually write this method
-        return true;
+        for (int i = 0; i < input.length; i++) {
+            input[i] *= input[i];
+        }
+        Arrays.sort(input);
+        Arrays.sort(squaredValues);
+        return Arrays.equals(input, squaredValues);
     }
 }
