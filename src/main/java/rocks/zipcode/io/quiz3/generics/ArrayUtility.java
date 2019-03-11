c45dcf1128aefbd0c54b8b9217ce5223e6f03c43
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-03-11 12:38:23 -0400

    refactored ArrayUtility

--- src/main/java/rocks/zipcode/io/quiz3/generics/ArrayUtility.java
+++ src/main/java/rocks/zipcode/io/quiz3/generics/ArrayUtility.java
@@ -1,6 +1,9 @@
 package rocks.zipcode.io.quiz3.generics;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 /**
  * @author leon on 09/12/2018.
@@ -13,18 +16,37 @@ public class ArrayUtility<SomeType> {
     }
 
     public SomeType findOddOccurringValue() {
+        int occurrences;
+        for (SomeType obj : array) {
+            occurrences = getNumberOfOccurrences(obj);
+            if (occurrences % 2 != 0) {
+                return obj;
+            }
+        }
         return null;
     }
 
     public SomeType findEvenOccurringValue() {
+        int occurrences;
+        for (SomeType obj : array) {
+            occurrences = getNumberOfOccurrences(obj);
+            if (occurrences % 2 == 0) {
+                return obj;
+            }
+        }
         return null;
     }
 
     public Integer getNumberOfOccurrences(SomeType valueToEvaluate) {
-        return null;
+        return (int) Arrays.stream(array).filter(x -> x.equals(valueToEvaluate)).count();
     }
 
+    @SuppressWarnings("unchecked")
     public SomeType[] filter(Function<SomeType, Boolean> predicate) {
-        return null;
+        Object[] objectArray = Arrays.stream(array)
+                .filter(predicate::apply)
+                .toArray(Object[]::new);
+
+        return (SomeType[]) Arrays.copyOf(objectArray, objectArray.length, array.getClass());
     }
 }
